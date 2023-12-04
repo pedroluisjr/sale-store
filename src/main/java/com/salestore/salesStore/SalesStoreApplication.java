54a65e3fe2286a00eb8c1fe21a04e923b29f93ed
Pedro Luiz Jr <pedro-luisjunior@hotmail.com>
2023-12-04 20:56:50 -0300

    first commit

---
+++ src/main/java/com/salestore/salesStore/SalesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.salestore.salesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesStoreApplication.class, args);
+	}
+
+}
